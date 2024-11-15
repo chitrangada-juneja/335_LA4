b9e96d8822e50cea8fb5cec7ae269cb198102172
Aidan Tucker <97992481+artboy110702@users.noreply.github.com>
2024-11-15 01:43:09 -0700

    Update MyLibraryTest.java
    This is what I last was testing when my eclipse was working. There are some errors in the implementation of my attempts to simulate the System.in input, and I would love any suggestions on better ideas, since this is all that I could figure to do after like a dozen hours of research. Obviously, the actual assertions need to be written, but that will take like 10 minutes, once the System.in part is figured out, it's just a matter of simulating user input that tripped me up.

--- MyLibraryTest.java
+++ MyLibraryTest.java
@@ -2,12 +2,16 @@
  * Note: Simulates a keyboard input by setting System.in
  * and .out via byte arrays.
  */
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.Assert.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 
-import java.io.*;
-
-import org.junit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class MyLibraryTest {
 
@@ -46,10 +50,18 @@ public class MyLibraryTest {
 
   @Test
   public void testExitFirst() {
-    final String testInput = "exit";
+    final String testInput = "addBook\nThe Hobbit\nJRR "
+    		+ "Tolkien\naddBook\nThe Sorcerer's Stone\nJK"
+    		+ " Rowling\naddBooks\nbooks.txt\nsuggestRead"
+    		+ "\ngetBooks\ntitle\ngetBooks\nauthor\nrate\n"
+    		+ "Where the Sidewalk Ends\nShel Silverstein\n"
+    		+ "5\nrate\nThe Hobbit\nJRR Tolkien\n5\nsetToRead"
+    		+ "\nWhere the Sidewalk Ends\nShel Silverstein"
+    		+ "\nsetToRead\nThe Hobbit\nJRR Tolkien\nexit";
     giveIn(testInput);
 
     MyLibrary.main(new String[0]);
-
-    assertEquals(testInput, getOut());
+    
+    
   }
+}
